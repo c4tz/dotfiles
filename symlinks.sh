3d1ae0aa285ff91b7d2985f01c7233c9e3791380
c4tz <j.zink@outlook.com>
2018-06-01 14:45:04 +0200

    pc

--- symlinks.sh
+++ symlinks.sh
@@ -11,6 +11,8 @@ mkdir -p $HOME/.config/gtk-3.0
 ln -s $HOME/.dotfiles/bspwm/bspwmrc $HOME/.config/bspwm/bspwmrc
 ln -s $HOME/.dotfiles/sxhkd/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
 ln -s $HOME/.dotfiles/polybar/config $HOME/.config/polybar/config
+ln -s $HOME/.dotfiles/polybar/crypto-config $HOME/.config/polybar/crypto-config
+ln -s $HOME/.dotfiles/polybar/crypto.py $HOME/.config/polybar/crypto.py
 ln -s $HOME/.dotfiles/rofi/config $HOME/.config/rofi/config
 ln -s $HOME/.dotfiles/xfce4/terminal/terminalrc $HOME/.config/xfce4/terminal/terminalrc
 ln -s $HOME/.dotfiles/Code/User/settings.json $HOME/.config/Code/User/settings.json
