e5816ab6853a0a9225ab4d89f9d57ed71df5afef
c4tz <j.zink@outlook.com>
2019-04-12 18:11:20 +0200

    fix sound script on wake up from suspend

--- .scripts/sound.sh
+++ .scripts/sound.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 if [ $1 = "headset" ]; then
-	device="alsa_output.usb-Logitech_Logitech_G930_Headset-00.analog-stereo"
+    device="alsa_output.usb-Logitech_Logitech_G930_Headset-00.analog-stereo"
 fi
 
 if [ $1 = "speakers" ]; then
-	device="alsa_output.pci-0000_00_1f.3.analog-surround-21"
+    pacmd set-card-profile 2 output:analog-surround-21+input:analog-stereo
+    device="alsa_output.pci-0000_00_1f.3.analog-surround-21"
 fi
 
 pacmd set-default-sink $device >/dev/null 2>&1
 
 for playing in $(pacmd list-sink-inputs | awk '$1 == "index:" {print $2}')
 do
-	pacmd move-sink-input $playing $device >/dev/null 2>&1
+    pacmd move-sink-input $playing $device >/dev/null 2>&1
 done
