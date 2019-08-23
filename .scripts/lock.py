df675a103f2855f03166ecb144523f89b3de518c
Jaro <j.zink@outlook.com>
2019-08-23 16:41:12 +0200

    support random monitor positions in lock screen

--- .scripts/lock.py
+++ .scripts/lock.py
@@ -7,34 +7,28 @@ import socket
 import sys
 
 from PIL import Image
-from screeninfo import get_monitors
 
-monitors = get_monitors()
 radius = 127 if socket.gethostname() == "pc" else 123
 
-root_w = sum(map(lambda x: x.width, monitors))
-root_h = max(map(lambda x: x.height, monitors))
-
-# take screenshot
 with mss.mss() as sct:
-    monitor = {'top': 0, 'left': 0, 'width': root_w, 'height': root_h}
-    sct_img = sct.grab(monitor)
+    # take screenshot
+    sct_img = sct.grab(sct.monitors[0])
     img = Image.frombytes('RGB', sct_img.size, sct_img.bgra, 'raw', 'BGRX')
 
-# pixelate
-imgSmall = img.resize(
-    (int(img.size[0] / 10), int(img.size[1] / 10)), resample=Image.BILINEAR
-)
+    # pixelate
+    imgSmall = img.resize(
+        (int(img.size[0] / 10), int(img.size[1] / 10)), resample=Image.BILINEAR
+    )
 
-result = imgSmall.resize(img.size, Image.NEAREST)
-icon = Image.open(sys.argv[1])
+    result = imgSmall.resize(img.size, Image.NEAREST)
+    icon = Image.open(sys.argv[1])
 
-# calc lock icon position on primary display
-primary = monitors[0]
-area = (
-    int(((primary.width / 2) + primary.x) - icon.size[0] / 2),
-    int(((primary.height / 2) + primary.y) - icon.size[1] / 2),
-)
+    # calc lock icon position on primary display
+    primary = sct.monitors[1]
+    area = (
+        int(((primary['width'] / 2) + primary['left']) - icon.size[0] / 2),
+        int(((primary['height'] / 2) + primary['top']) - icon.size[1] / 2),
+    )
 
 # add lock icon to bg image
 result.paste(icon, area, icon)
