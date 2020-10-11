22b14f5d74c209a0564fc0628a30da8baca7eea7
c4tz <j.zink@outlook.com>
2020-10-11 19:25:51 +0200

    also re-color rambox in normal mode

--- .scripts/rambox.sh
+++ .scripts/rambox.sh
@@ -6,6 +6,7 @@ CSSFILE=$TMPFOLDER/resources/Rambox-all.css
 
 npx asar extract $ASARFILE $TMPFOLDER
 
+echo ".x-tab-bar-default { background-color: #333 !important }" >> $CSSFILE
 echo ".x-tab-bar-default.dontdisturb { background-color: #333 !important }" >> $CSSFILE
 echo ".x-tab { background-color: #555 !important }" >> $CSSFILE
 echo ".x-tab.x-tab-active { background-color: #777 !important }" >> $CSSFILE
