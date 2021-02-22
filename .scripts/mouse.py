b574dda0d83604e09adbe7d983e86e6386d3c3c3
c4tz <j.zink@outlook.com>
2021-02-22 13:04:36 +0100

    add min value to mouse script

--- .scripts/mouse.py
+++ .scripts/mouse.py
@@ -11,7 +11,8 @@ result = Popen(
 
 mv = int(re.search(r"(Battery: )([0-9]+)(mV)", result.decode()).group(2))
 
-# hardcoded because there is no queryable max value
-percent = round(mv / 4211 * 100)
+# hardcoded because there is no queryable min/max value
+min, max = 3500, 4211
+percent = round((mv - min) / (max - min) * 100)
 
 print(f"{percent}%")
